21038c855a9deea01041fdd0ef280e9f75a1c3f8
Sahil <112234265+amicable-sahil@users.noreply.github.com>
2022-08-27 19:50:31 +0530

    Add files via upload

---
+++ Assignment_04.py
@@ -0,0 +1,57 @@
+# Q1:
+x = input("Enter your name")
+print(x)
+
+# Q2:
+num = int(input("Enter a number"))
+print(num)
+print(type(num))
+
+# Q3:
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print("The sum is =", sum)
+
+# Q4:
+radius = int(input("Enter radius"))
+circleArea = (22/7)*(radius**2)
+print(circleArea)
+
+#Q5:
+num = int(input("Enter any number"))
+squareNum = num**2
+print(squareNum)
+
+#Q6:
+base = int(input("Enter the base value"))
+height = int(input("Enter height"))
+triangleArea = 1/2 * base * height
+print(triangleArea)
+
+#Q7:
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+num3 = int(input("Enter third number"))
+avarage = (num1 + num2 + num3)/3
+print(avarage)
+
+#Q8:
+p = int(input("Enter principle amount"))
+r = int(input("Enter rate"))
+t = int(input("Enter time"))
+interest = (p*r*t)/100
+print("The simple interest is =", interest)
+
+#Q9:
+length = int(input("Enter lenght"))
+height = int(input("Enter height"))
+width = int(input("Enter width"))
+cubicVolume = length * height * width
+print("The volume of cubic is =", cubicVolume)
+
+#Q10:
+length = int(input("Enter lenght"))
+bredth = int(input("Enter bredth"))
+rectArea = length * bredth
+print("The area of rectangle is =", rectArea)
